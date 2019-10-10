fca334a55d646f8223771ae2fdf33a9a07367654
Jim Rush <jim@galesburg.co.nz>
2019-10-11 09:42:36 +1300

    Add green hues & lint

--- landscape.py
+++ landscape.py
@@ -6,14 +6,15 @@
 # version : 0.1.0
 
 
-import os                             # path resolving and image saving
-import random                         # midpoint displacement
-from PIL import Image, ImageDraw      # image creation and drawing
-import bisect                         # working with the sorted list of points
+import os  # path resolving and image saving
+import random  # midpoint displacement
+from PIL import Image, ImageDraw  # image creation and drawing
+import bisect  # working with the sorted list of points
 
 # Iterative midpoint vertical displacement
-def midpoint_displacement(start, end, roughness, vertical_displacement=None,
-                          num_of_iterations=16):
+def midpoint_displacement(
+    start, end, roughness, vertical_displacement=None, num_of_iterations=16
+):
     """
     Given a straight line segment specified by a starting point and an endpoint
     in the form of [starting_point_x, starting_point_y] and [endpoint_x, endpoint_y],
@@ -26,7 +27,7 @@ def midpoint_displacement(start, end, roughness, vertical_displacement=None,
     if vertical_displacement is None:
         # if no initial displacement is specified set displacement to:
         #  (y_start+y_end)/2
-        vertical_displacement = (start[1]+end[1])/2
+        vertical_displacement = (start[1] + end[1]) / 2
     # Data structure that stores the points is a list of lists where
     # each sublist represents a point and holds its x and y coordinates:
     # points=[[x_0, y_0],[x_1, y_1],...,[x_n, y_n]]
@@ -42,14 +43,16 @@ def midpoint_displacement(start, end, roughness, vertical_displacement=None,
         # the original sequence.
         # Tuple type is used for security reasons since they are immutable in Python.
         points_tup = tuple(points)
-        for i in range(len(points_tup)-1):
+        for i in range(len(points_tup) - 1):
             # Calculate x and y midpoint coordinates:
             # [(x_i+x_(i+1))/2, (y_i+y_(i+1))/2]
-            midpoint = list(map(lambda x: (points_tup[i][x]+points_tup[i+1][x])/2,
-                                [0, 1]))
+            midpoint = list(
+                map(lambda x: (points_tup[i][x] + points_tup[i + 1][x]) / 2, [0, 1])
+            )
             # Displace midpoint y-coordinate
-            midpoint[1] += random.choice([-vertical_displacement,
-                                          vertical_displacement])
+            midpoint[1] += random.choice(
+                [-vertical_displacement, vertical_displacement]
+            )
             # Insert the displaced midpoint in the current list of points
             bisect.insort(points, midpoint)
             # bisect allows to insert an element in a list so that its order
@@ -62,36 +65,74 @@ def midpoint_displacement(start, end, roughness, vertical_displacement=None,
         iteration += 1
     return points
 
+
 def generate_random_palette():
-    dicts = [{'0': (173, 189, 209), '1': (141, 163, 191),
-                      '2': (108, 136, 172), '3': (83, 110, 147), '4': (64, 85, 114),
-                      '5': (46, 61, 82), '6': (239, 242, 246)},
-                      {'0': (195, 157, 224), '1': (158, 98, 204),
-                      '2': (130, 79, 138), '3': (68, 28, 99), '4': (49, 7, 82),
-                      '5': (23, 3, 38), '6': (240, 203, 163)}]
+    dicts = [
+        {
+            "0": (173, 189, 209),
+            "1": (141, 163, 191),
+            "2": (108, 136, 172),
+            "3": (83, 110, 147),
+            "4": (64, 85, 114),
+            "5": (46, 61, 82),
+            "6": (239, 242, 246),
+        },
+        {
+            "0": (195, 157, 224),
+            "1": (158, 98, 204),
+            "2": (130, 79, 138),
+            "3": (68, 28, 99),
+            "4": (49, 7, 82),
+            "5": (23, 3, 38),
+            "6": (240, 203, 163),
+        },
+        {
+            "0": (22, 164, 39),
+            "1": (8, 136, 24),
+            "2": (0, 112, 14),
+            "3": (0, 83, 10),
+            "4": (64, 85, 114),
+            "5": (46, 61, 82),
+            "6": (83, 110, 147),
+        },
+    ]
     return random.choice(dicts)
 
-# Strahan
-                    #   color_dict = {'0': (173, 189, 209), '1': (141, 163, 191),
-                    #   '2': (108, 136, 172), '3': (83, 110, 147), '4': (64, 85, 114),
-                    #   '5': (46, 61, 82), '6': (239, 242, 246)}
+    # # Strahan
+    # strahan = {
+    #     "0": (173, 189, 209),
+    #     "1": (141, 163, 191),
+    #     "2": (108, 136, 172),
+    #     "3": (83, 110, 147),
+    #     "4": (64, 85, 114),
+    #     "5": (46, 61, 82),
+    #     "6": (239, 242, 246),
+    # }
 
-# Caitlin Spice
+    # Caitlin Spice
+    # caitlin_spice = {
+    #     "0": (22, 164, 39),
+    #     "1": (8, 136, 24),
+    #     "2": (0, 112, 14),
+    #     "3": (0, 83, 10),
+    #     "4": (64, 85, 114),
+    #     "5": (46, 61, 82),
+    #     "6": (55, 94, 147),
+    # }
+
 
 def draw_layers(layers, width, height, color_dict=None):
-    # Default color palette
-
-    #'6' is sky
+    #'6' is sky, #0->#4 is layers
 
     if color_dict is None:
-          color_dict = generate_random_palette()
+        color_dict = generate_random_palette()
     else:
         # len(color_dict) should be at least: # of layers +1 (background color)
-        if len(color_dict) < len(layers)+1:
+        if len(color_dict) < len(layers) + 1:
             raise ValueError("Num of colors should be bigger than the num of layers")
 
     # Create image into which the terrain will be drawn
-    landscape = Image.new('RGBA', (width, height), color_dict[str(len(color_dict)-1)])
+    landscape = Image.new("RGBA", (width, height), color_dict[str(len(color_dict) - 1)])
     landscape_draw = ImageDraw.Draw(landscape)
     # Draw the sun
     landscape_draw.ellipse((50, 25, 100, 75), fill=(255, 255, 255, 255))
@@ -99,29 +140,39 @@ def draw_layers(layers, width, height, color_dict=None):
     final_layers = []
     for layer in layers:
         sampled_layer = []
-        for i in range(len(layer)-1):
+        for i in range(len(layer) - 1):
             sampled_layer += [layer[i]]
             # If x difference is greater than 1
-            if layer[i+1][0]-layer[i][0] > 1:
+            if layer[i + 1][0] - layer[i][0] > 1:
                 # Linearly sample the y values in the range x_[i+1]-x_[i]
                 # This is done by obtaining the equation of the straight
                 # line (in the form of y=m*x+n) that connects two consecutive
                 # points
-                m = float(layer[i+1][1]-layer[i][1])/(layer[i+1][0]-layer[i][0])
-                n = layer[i][1]-m*layer[i][0]
-                r = lambda x: m*x+n  # straight line
-                for j in range(int(layer[i][0]+1), int(layer[i+1][0])):  # for all missing x
+                m = float(layer[i + 1][1] - layer[i][1]) / (
+                    layer[i + 1][0] - layer[i][0]
+                )
+                n = layer[i][1] - m * layer[i][0]
+                r = lambda x: m * x + n  # straight line
+                for j in range(
+                    int(layer[i][0] + 1), int(layer[i + 1][0])
+                ):  # for all missing x
                     sampled_layer += [[j, r(j)]]  # Sample points
         final_layers += [sampled_layer]
 
     final_layers_enum = enumerate(final_layers)
     for final_layer in final_layers_enum:
         # traverse all x values in the layer
-        for x in range(len(final_layer[1])-1):
+        for x in range(len(final_layer[1]) - 1):
             # for each x value draw a line from its y value to the bottom
-            landscape_draw.line((final_layer[1][x][0], height-final_layer[1][x][1],
-                                 final_layer[1][x][0], height),
-                                 color_dict[str(final_layer[0])])
+            landscape_draw.line(
+                (
+                    final_layer[1][x][0],
+                    height - final_layer[1][x][1],
+                    final_layer[1][x][0],
+                    height,
+                ),
+                color_dict[str(final_layer[0])],
+            )
 
     return landscape
 
@@ -136,8 +187,6 @@ def generate_landscape():
     layer_4 = midpoint_displacement([0, 350], [width, 320], 0.9, 90, 8)
 
     landscape = draw_layers([layer_4, layer_3, layer_2, layer_1], width, height)
-    # landscape.save(os.getcwd()+'/testing.png')
+    landscape.save(os.getcwd() + "/testing.png")
     # print("Generating" + landscape)
     return landscape
-
-
